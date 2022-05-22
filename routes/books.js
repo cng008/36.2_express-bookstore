bdd60c91c4e8980c4c7373a3b18e78e1c62372f0
Christien Ng <ngchristien@gmail.com>
2022-05-22 13:49:31 -0700

    add JSONSchema validation for creation and updating of books

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,15 @@
-const express = require("express");
-const Book = require("../models/book");
-
+const express = require('express');
 const router = new express.Router();
 
+const { validate } = require('jsonschema');
+const bookSchemaNew = require('../schemas/bookSchemaNew');
+const bookSchemaUpdate = require('../schemas/bookSchemaUpdate');
+
+const Book = require('../models/book');
 
 /** GET / => {books: [book, ...]}  */
 
-router.get("/", async function (req, res, next) {
+router.get('/', async function (req, res, next) {
   try {
     const books = await Book.findAll(req.query);
     return res.json({ books });
@@ -17,7 +20,7 @@ router.get("/", async function (req, res, next) {
 
 /** GET /[id]  => {book: book} */
 
-router.get("/:id", async function (req, res, next) {
+router.get('/:id', async function (req, res, next) {
   try {
     const book = await Book.findOne(req.params.id);
     return res.json({ book });
@@ -28,8 +31,18 @@ router.get("/:id", async function (req, res, next) {
 
 /** POST /   bookData => {book: newBook}  */
 
-router.post("/", async function (req, res, next) {
+router.post('/', async function (req, res, next) {
   try {
+    const validation = validate(req.body, bookSchemaNew);
+    if (!validation.valid) {
+      // pass validation errors to error handler
+      return next({
+        status: 400,
+        errors: validation.errors.map(e => e.stack)
+      });
+    }
+
+    // at this point in code, we know we have a valid payload
     const book = await Book.create(req.body);
     return res.status(201).json({ book });
   } catch (err) {
@@ -39,8 +52,24 @@ router.post("/", async function (req, res, next) {
 
 /** PUT /[isbn]   bookData => {book: updatedBook}  */
 
-router.put("/:isbn", async function (req, res, next) {
+router.put('/:isbn', async function (req, res, next) {
   try {
+    if ('isbn' in req.body) {
+      return next({
+        status: 400,
+        message: 'Not allowed'
+      });
+    }
+    const validation = validate(req.body, bookSchemaUpdate);
+    if (!validation.valid) {
+      // pass validation errors to error handler
+      return next({
+        status: 400,
+        errors: validation.errors.map(e => e.stack)
+      });
+    }
+
+    // at this point in code, we know we have a valid payload
     const book = await Book.update(req.params.isbn, req.body);
     return res.json({ book });
   } catch (err) {
@@ -50,10 +79,10 @@ router.put("/:isbn", async function (req, res, next) {
 
 /** DELETE /[isbn]   => {message: "Book deleted"} */
 
-router.delete("/:isbn", async function (req, res, next) {
+router.delete('/:isbn', async function (req, res, next) {
   try {
     await Book.remove(req.params.isbn);
-    return res.json({ message: "Book deleted" });
+    return res.json({ message: 'Book deleted' });
   } catch (err) {
     return next(err);
   }
